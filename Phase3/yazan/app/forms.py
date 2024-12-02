e95b9b36ebca6ecf37735fe12222048d208c5cec
yazan AbuAloun <1210145@student.birzeit.edu>
2024-12-02 09:29:41 +0200

    Added strong password requirement

--- Phase3/yazan/app/forms.py
+++ Phase3/yazan/app/forms.py
@@ -2,12 +2,25 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 from wtforms.fields.simple import PasswordField
 from wtforms.validators import DataRequired, Email
+from wtforms.validators import Regexp
+
+
+class StrongPassword:
+    def __init__(self, message=None):
+        if not message:
+            message = 'Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.'
+        self.message = message
+
+    def __call__(self, form, field):
+        pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+        validator = Regexp(pattern, message=self.message)
+        validator(form, field)
 
 class CustomerForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), StrongPassword()])
     submit = SubmitField('Sign up')
 
 class LoginForm(FlaskForm):
